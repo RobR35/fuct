d8a561721e49af520fb704e5df3661c2ba9ca127
Rob Rawnsley <robrawnsley@gmail.com>
2018-05-10 22:07:22 +1200

    Add functionality to generate Srecord string.

--- src/main/python/fuct/srecord.py
+++ src/main/python/fuct/srecord.py
@@ -45,3 +45,49 @@ class SRecord(object):
             raise TypeError('Paging in %s records is not supported or not enough address bytes' % self.stype[0])
 
         return unpack_from('>H', self.address[-2:])[0]
+
+    def print_srec(self):
+        def srec_length():
+            # Record length field includes address, data and checksum bytes
+            address_length = self.stype[1]
+            data_length = 0
+            checksum_length = 1
+
+            # Calculate data length if data is present
+            if self.stype[2] and self.data is not None:
+                data_length = int(len(self.data) // 2)
+
+            return address_length + data_length + checksum_length
+
+        def srec_checksum():
+            # Checksum is calculated over length, address and data fields
+            if len(self.address) != self.stype[1]:
+                raise TypeError('Wrong number of address bytes for %s record type' % self.stype[0])
+
+            checksum = srec_length()
+
+            for address_byte in self.address:
+                checksum += int(address_byte)
+
+            # Checksum the data if present for this record type
+            if self.stype[2]:
+                rec_data = self.data
+                while len(rec_data):
+                    checksum += int(rec_data[:2], 16)
+                    rec_data = rec_data[2:]
+
+            checksum = int(255) - checksum
+            checksum = checksum & int(255)
+
+            return checksum
+
+        srec_string = self.stype[0]
+        srec_string += hex(srec_length()).upper()[2:].zfill(2)
+        srec_string += str(binascii.hexlify(self.address)).upper()
+
+        if self.stype[2] and self.data is not None:
+            srec_string += str(self.data)
+
+        srec_string += hex(srec_checksum())[2:].zfill(2).upper()
+
+        return srec_string
